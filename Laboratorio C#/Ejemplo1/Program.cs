f1c36b2ee5aa78eea2208281972badf2119be944
Kevin Vazquez <kgvazquez05@gmail.com>
2024-03-05 09:49:19 -0500

    "Lots of new stuff"

---
+++ Laboratorio C#/Ejemplo1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVariable = 10;
+            const int myConstant = 20;
+
+            Console.WriteLine("Valor de mi variable: ",myVariable)
+
+            Console.WriteLine("Valor de mi constante: ",myConstant)
+        }
+    }
+}
